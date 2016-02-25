88037f5f96be77c231859454c0eaaa1b56564b94
Adam Berecz <adam@webaxis.hu>
2016-02-25 15:58:36 +0100

    first commit

---
+++ src/RegistryServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Waxis\Registry;
+
+use Illuminate\Support\Facades\App;
+use Illuminate\Support\ServiceProvider;
+
+class RegistryServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+}
